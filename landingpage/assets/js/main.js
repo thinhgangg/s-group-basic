0de5fad0e1b9d8ba6497814affb03d168cc4080c
Phuc Thinh <phucthinhmai00@gmail.com>
2026-07-08 19:32:40 +0700

    feat: add animations

--- landingpage/assets/js/main.js
+++ landingpage/assets/js/main.js
@@ -1,24 +1,73 @@
 $(document).ready(function () {
-  // Script cũ của slider giữ nguyên ở đây...
+  $(".banner-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 
-  // TỰ ĐỘNG XỬ LÝ TRÀN MENU CON
-  $(".mega-item-has-child")
-    .on("mouseenter", function () {
-      var $childBox = $(this).find(".mega-child-box");
+  $(".ecosystem-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 
-      // Lấy tọa độ mép phải của hộp menu con (Khoảng cách từ mép trái + Chiều rộng hộp)
-      var boxRightEdge = $childBox.offset().left + $childBox.outerWidth();
+  $(".projects-slider").slick({
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: true,
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+        },
+      },
+    ],
+  });
 
-      // Lấy chiều rộng của toàn bộ cửa sổ trình duyệt
-      var windowWidth = $(window).width();
+  const animatedElements = document.querySelectorAll(
+    ".fade-up, .fade-right, .fade-left",
+  );
 
-      // Nếu mép phải của hộp vượt ra khỏi màn hình (cộng thêm 20px khoảng lùi cho an toàn)
-      if (boxRightEdge + 20 > windowWidth) {
-        $(this).addClass("open-left"); // Tự động mở sang trái
-      }
-    })
-    .on("mouseleave", function () {
-      // Xóa class khi đưa chuột ra chỗ khác để reset trạng thái
-      $(this).removeClass("open-left");
-    });
+  const observer = new IntersectionObserver(
+    (entries, observer) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          observer.unobserve(entry.target);
+        }
+      });
+    },
+    {
+      threshold: 0.1,
+      rootMargin: "0px 0px -30px 0px",
+    },
+  );
+
+  animatedElements.forEach((el) => observer.observe(el));
 });
